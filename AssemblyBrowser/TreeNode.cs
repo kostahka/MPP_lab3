01af40533ed0a67ec82b0c39e0cdcd7614a1b607
Max <maks_kosten@mail.ru>
2022-11-10 01:15:42 +0300

    Basic reprasentation of nodes

---
+++ AssemblyBrowser/TreeNode.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyBrowser
+{
+    class TreeNode
+    {
+        public string name { get; }
+        public List<TreeNode> childs { get; set; }
+
+        public TreeNode(string name)
+        {
+            childs = new List<TreeNode>();
+            this.name = name;
+        }
+
+        public void AddChild(TreeNode node)
+        {
+            childs.Add(node);
+        }
+    }
+}
